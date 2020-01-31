227293a9e6993b9e0e2ae083e38a6fb15b75022f
essiervo <58839850+essiervo@users.noreply.github.com>
2020-01-31 12:27:26 +0800

    LAB02_ON
    Not final

---
+++ LAB02_ON/AdjacencyMatrix.java
@@ -0,0 +1,147 @@
+import java.util.*;
+
+public class AdjacencyMatrix{
+	Scanner scan = new Scanner(System.in);
+	int vertex, v1, v2, n;
+	int [][] graph;
+
+	public AdjacencyMatrix(int vertex){
+		this.graph = new int[vertex][vertex];
+		this.vertex = vertex;
+	}
+
+	public int getNumberOfVertices(){
+		return graph.length;
+	}
+
+	public int getNumberOfEdges(){
+		int num = 0;
+		for(int i=0; i<graph.length; i++){
+			for(int j = 0; j<graph[i].length; j++){
+				if(graph[i][j]==1 && graph[j][i]==1){
+					num++;
+				}
+			}
+		}
+		return num;
+	}
+
+	public void insertingEdge(){
+		n = graph.length;
+		System.out.print("1st vertex: ");
+		v1 = scan.nextInt();
+
+		if(v1>=n || v1<0){ 
+            System.out.println("Vertex not present!"); 
+            return; 
+        }
+
+		System.out.print("2nd vertex: ");
+		v2 = scan.nextInt();
+
+		if(v2>=n || v2<0){ 
+            System.out.println("Vertex not present!"); 
+            return; 
+        }
+
+		graph[v1][v2] = 1;
+		graph[v2][v1] = 1;
+	}
+
+	public void removingEdge(){
+		System.out.print("1st vertex: ");
+		v1 = scan.nextInt();
+
+		if(v1>n || v1<0){ 
+            System.out.println("Vertex not present!"); 
+            return; 
+        }
+
+		System.out.print("2nd vertex: ");
+		v2 = scan.nextInt();
+
+		if(v2>n || v2<0){ 
+            System.out.println("Vertex not present!"); 
+            return; 
+        }
+
+		graph[v1][v2] = 0;
+		graph[v2][v1] = 0;
+	}
+
+	public void vertexAdjacent(int vertex){
+		System.out.print("Vertices adjacent to a given vertex: ");
+		for(int i=0; i<graph.length; i++){
+			if(graph[vertex][i] == 1 && graph[i][vertex] == 1){
+				System.out.print(vertex + " ");
+			}
+		}
+		System.out.print("\n");
+	}
+
+	public void twoAdjVertex(int vertex1, int vertex2){
+		if(graph[vertex1][vertex2] == 1 && graph[vertex2][vertex1] == 1)
+			System.out.print("The two vertices are adjacent!\n");
+		else
+			System.out.print("The two vertices are NOT adjacent!\n");	
+	}
+
+	/*public void twoConVertex(int vertex1, int vertex2, int num){
+		if(graph[vertex1][num] != 1){
+			twoConVertex(vertex1, num++, )
+		}
+	}*/
+
+
+	public void showGraph(){
+		System.out.print("UGraph \n");
+		for(int i=0; i<graph.length; i++){
+			System.out.print("[ ");
+			for(int j = 0; j<graph[i].length; j++){
+				System.out.print(graph[i][j] + " ");
+			}
+			System.out.print("]\n");
+		}
+	}
+
+	public void depthFirst(){
+		Stack<Integer> stack = new Stack<Integer>();
+	}
+
+	public void breadthFirst(){
+		Queue<Integer> queue = new LinkedList<Integer>();
+	}
+
+	public void insertVertex(){
+		n = graph.length;
+		this.graph = new int[n++][n++];
+        n++; 
+        for(int i=0; i<n; i++){ 
+            graph[i][n] = 0; 
+            graph[n][i] = 0; 
+        } 
+    } 
+  
+    public void removeVertex(int vertex){ 
+        n = graph.length;
+        if (vertex > n){ 
+            System.out.println("Vertex not present!"); 
+            return; 
+        } 
+
+        else { 
+            while(vertex<n) 
+            { 
+                for(int i=0; i<n; ++i){ 
+                    graph[i][vertex] = graph[i][vertex+1]; 
+                } 
+  
+                for (int i = 0; i<n; ++i){ 
+                    graph[vertex][i] = graph[vertex+1][i]; 
+                } 
+                vertex++; 
+            } 
+            n--; 
+        }
+    }
+}
---
+++ LAB02_ON/UGraph.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class UGraph{
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+		int choice, v, v1, v2;
+
+		System.out.print("Enter number of vertices: ");
+		v = scan.nextInt();
+
+		AdjacencyMatrix adjMatrix = new AdjacencyMatrix(v);
+
+		//adjMatrix.showGraph();
+
+		do{
+			System.out.print("\n1. Inquire \n2. Modify \n3. Traverse \n4. Show Matrix \n5. Exit");
+			System.out.print("\n\nEnter choice: ");
+
+			choice = scan.nextInt();
+
+			switch(choice){
+				case 1:
+					do{
+						System.out.print("\n1. Number of vertices \n2. Number of edges \n3. Vertices adjacent to a given vertex \n4. Are two vertices adjacent? \n5. Are two vertices connected? \n6. Back");
+						System.out.print("\n\nEnter choice: ");
+						choice = scan.nextInt();
+			
+						switch(choice){
+							case 1:
+								System.out.print("Number of vertices: " + adjMatrix.getNumberOfVertices() + "\n");
+								break;
+
+							case 2:
+								System.out.print("Number of edges: " + adjMatrix.getNumberOfEdges() + "\n");
+								break;
+
+							case 3:
+								System.out.print("Vertex: ");
+								v = scan.nextInt();
+								adjMatrix.vertexAdjacent(v);
+								break;
+
+							case 4:
+								System.out.print("1st Vertex: ");
+								v1 = scan.nextInt();
+								System.out.print("2nd Vertex: ");
+								v2 = scan.nextInt();
+								adjMatrix.twoAdjVertex(v1, v2);
+								break;
+
+							case 5:
+
+								break;
+						}
+					}while(choice != 6);
+					break;
+
+				case 2:
+					do{
+						System.out.print("\n1. Insert an edge \n2. Remove an edge \n3. Insert a vertex \n4. Remove a vertex \n5. Back");
+						System.out.print("\n\nEnter choice: ");
+						choice = scan.nextInt();
+			
+						switch(choice){
+							case 1:
+								adjMatrix.insertingEdge();
+								break;
+
+							case 2:
+								adjMatrix.removingEdge();
+								break;
+
+							case 3:
+								adjMatrix.insertVertex();
+								break;
+
+							case 4:
+								System.out.print("Enter vertext you want to remove: ");
+								adjMatrix.removeVertex(scan.nextInt());
+								break;
+						}
+					}while(choice != 5);
+					break;
+
+				case 3:
+					do{
+						System.out.print("\n1. Depth-First Search \n2. Breadth-First Search \n3. Exit");
+						System.out.print("\n\nEnter choice: ");
+						choice = scan.nextInt();
+			
+						switch(choice){
+							case 1:
+								
+							case 2:
+									
+						}
+					}while(choice!=3);
+					break;
+
+				case 4: 
+					adjMatrix.showGraph();
+					break;
+			}
+		}while(choice != 5);
+	}
+}
+
+
+
